06aace97eb8c9f9e3e688203da1b0c16f57d6c2d
kiana <kianakermani2001@gmail.com>
2024-02-26 10:53:09 +0330

    add footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Weather App</h1>;
+  return (
+    <div>
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/kianakermani"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Kiana Kermani
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/kianakermani/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>{" "}
+        on GitHub
+      </footer>
+    </div>
+  );
 }
