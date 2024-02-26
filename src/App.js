f540afda0de632e32449a3e7349eb4fa68063efd
kiana <kianakermani2001@gmail.com>
2024-02-26 14:24:39 +0330

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,27 +2,29 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div>
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://github.com/kianakermani"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Kiana Kermani
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/kianakermani/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>{" "}
-        on GitHub
-      </footer>
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/kianakermani"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Kiana Kermani
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/kianakermani/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>{" "}
+          on GitHub
+        </footer>
+      </div>
     </div>
   );
 }
